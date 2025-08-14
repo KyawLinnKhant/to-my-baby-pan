2f76012909478fd1cb76122207d90ac60bd3fdea
awnnant <klkhant707@gmail.com>
2025-08-15 03:45:19 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Editable config
+window.APP_CONFIG = {
+  lovingSince: "2022-10-18",        // YYYY-MM-DD
+  knightPassword: "pan❤️2025",      // change as you like
+  cloudFolder: "to-my-princess-pan" // not used here, but reserved
+};
